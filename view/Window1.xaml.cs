353aadc47a15993e6ffb6534b68e5f69b2e950b8
Cristhofer Parada <cristhofer.parada@usach.cl>
2021-08-21 23:43:25 -0400

    Add files via upload

---
+++ view/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab4.view
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_bloque1_Click(object sender, RoutedEventArgs e)
+        {
+            TB_bloque1.Text = "la wea que querai";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TB_bloque1.Text = "la wea que querai2222";
+        }
+    }
+}
